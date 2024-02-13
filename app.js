2d21c131928654d5549018afba36bad8e25721b5
Lawrence <143608054+llawrencee@users.noreply.github.com>
2024-02-13 20:28:11 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require("fs")
+const { Server } = require("socket.io")
+
+const io = new Server({
+  cors: {
+    origin: "*",
+  },
+})
+
+io.on("connection", (socket) => {
+  socket.on("send_json", (info) => {
+    const messages_path = "./messages.json"
+    const messages = JSON.parse(fs.readFileSync(messages_path))
+
+    socket.broadcast.emit("json_response", messages.concat(info))
+    fs.writeFileSync("./messages.json", JSON.stringify(messages.concat(info)))
+
+    console.log(JSON.stringify(messages.concat(info)))
+    console.log(`writing to: './messages.json'`)
+  })
+
+  socket.on("receive_json", () => {
+    const messages_path = "./messages.json"
+    const messages = require(messages_path)
+    socket.emit("json_response", messages)
+  })
+})
+
+io.listen(3000)
